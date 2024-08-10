a195865181aab2289f3671272d49ad75b91f2e50
lxhillwind <lxhillwind@foxmail.com>
2024-08-10 10:23:51 +0800

    上传现有配置

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # rime-config
+
+## 本仓库包含如下内容
+
+### [lua/aux_code.lua](lua/aux_code.lua)
+- 实现了 **自定义短语** 功能, 用于以挂接辅助码的方式来支持编码方案 (例如小鹤音形, 五笔等);
+
+![自定义短语](pic/自定义短语.png)
+
+- 实现了 **直接辅助码** 功能, 将 "自定义短语" 中的编码第三位作为辅助码来筛选词语;
+    - 在输入编码为 5 / 7 / 9 时生效, 对应二字词 / 三字词 / 四字词 (更长的输入就没有必要了);
+    - 为了实现的简单, 辅助码仅支持 1 位;
+    - 辅助码支持词中任意字;
+
+![直接辅助码](pic/辅助码.png)
+
+- 这2个功能都是强制生效的; 如果不需要的话, 可以直接修改代码, 将对应分支的条件更改一下就行.
+
+- 由于版权因素, 本仓库并不提供 "自定义短语" 文件.
+
+### [double_pinyin_flypy.custom.yaml](double_pinyin_flypy.custom.yaml)
+- 个人配置, 其中包含了上述 lua 插件的使用说明;
+
+也引用如下:
+
+```yaml
+  # 需要放在 simplifier filter 之后
+  engine/filters/@after 0: lua_filter@*aux_code@all-utf8.ini
+```
+
+### [default.custom.yaml](default.custom.yaml)
+- 个人配置.
+
+## 本仓库未包含的文件
+
+### double_pinyin_flypy.schema.yaml
+- 来自 <https://github.com/rime/rime-double-pinyin> 仓库;
+
+### lua/all-utf8.ini
+- `编码,序号=字词` 格式的 "自定义短语" (不满足这个格式的行会被忽略);
+- 当然, 你可以将其改成其他文件名; 只要在方案中引用此插件时进行对应修改即可.
+
+## 致谢
+
+- <https://github.com/HowcanoeWang/rime-lua-aux-code> RIME输入法辅助码音形分离插件; 本仓库的 lua 代码改动自此.
---
+++ lua/aux_code.lua
@@ -0,0 +1,141 @@
+-- 改动自 https://github.com/HowcanoeWang/rime-lua-aux-code
+--
+-- ./origin-aux_code 包含了其原本的代码和版权信息.
+
+local AuxFilter = {}
+
+-- local log = require 'log'
+-- log.outfile = "/tmp/aux_code.log"
+
+function AuxFilter.init(env)
+    -- log.info("** AuxCode filter", env.name_space)
+
+    AuxFilter.aux_code, AuxFilter.code_map = AuxFilter.readAuxTxt(env.name_space)
+end
+
+----------------
+-- 閱讀輔碼文件 --
+----------------
+function AuxFilter.readAuxTxt(txtpath)
+    -- log.info("** AuxCode filter", 'read Aux code txt:', txtpath)
+
+    local userPath = rime_api.get_user_data_dir() .. "/lua/"
+    local fileAbsolutePath = userPath .. txtpath
+    local file = io.open(fileAbsolutePath, "r")
+    local auxCodes = {}
+    local codeMap = {}
+    if not file then
+        error("Unable to open auxiliary code file.")
+        return auxCodes, codeMap
+    end
+
+    for line in file:lines() do
+        line = line:match("[^\r\n]+") -- 去掉換行符，不然 value 是帶著 \n 的
+        local code, seq, ch = line:match("([a-z]+),([0-9]+)=(.+)")
+        if code and seq and ch then
+            seq = tonumber(seq)
+            if code:len() >= 3 and code:len() <= 4 then
+                local i = code:sub(3, 3)
+                if not auxCodes[ch] then
+                    auxCodes[ch] = ''
+                end
+                auxCodes[ch] = auxCodes[ch] .. i
+            end
+            if not codeMap[code] then
+                codeMap[code] = {}
+            end
+            codeMap[code][seq] = ch
+        end
+    end
+    file:close()
+    -- 確認 code 能打印出來
+    -- for key, value in pairs(AuxFilter.aux_code) do
+    --     log.info(key, table.concat(value, ','))
+    -- end
+
+    return auxCodes, codeMap
+end
+
+------------------
+-- filter 主函數 --
+------------------
+function AuxFilter.func(input, env)
+    local context = env.engine.context
+    local inputCode = context.input
+    local phraseMap = AuxFilter.code_map[inputCode]
+
+    -- 处理 "自定义短语"
+    if phraseMap then
+        local keys = {}
+        for i in pairs(phraseMap) do
+            table.insert(keys, i)
+        end
+        table.sort(keys)
+
+        local counter = 0
+        for cand in input:iter() do
+            counter = counter + 1
+            while phraseMap[counter] do
+                yield(Candidate('user_phrase', 0, #inputCode, phraseMap[counter], ''))
+                table.remove(keys, 1)
+                counter = counter + 1
+            end
+            yield(cand)
+        end
+        -- 如果输入法产生的候选词不够, 就会产生空洞;
+        -- 直接将剩下的自定义短语按照顺序生成 Candidate.
+        if #keys > 0 then
+            for _, i in ipairs(keys) do  -- 使用 ipairs, 确保顺序.
+                yield(Candidate('user_phrase', 0, #inputCode, phraseMap[i], ''))
+            end
+        end
+
+        return
+    end
+
+    -- 输入长度为大于4且小于10的奇数时 (对应2-4字词语), 进行辅助码筛选.
+    if not (#inputCode > 4 and #inputCode < 10 and #inputCode % 2 == 1) then
+        -- 直接yield所有待选项，不进入后续迭代，提升性能
+        for cand in input:iter() do
+            yield(cand)
+        end
+        return
+    else
+        local insertLater = {}
+        local lastChar = inputCode:match('.$')
+        local hasMatched = false
+        for cand in input:iter() do
+            local found = false
+            local testThis = utf8.len(cand.text) * 2 + 1 == #inputCode and cand._end + 1 == #inputCode
+
+            for _, codePoint in utf8.codes(cand.text) do
+                if testThis and not found then
+                    local char = utf8.char(codePoint)
+                    local charAuxCodes = AuxFilter.aux_code[char] -- 每個字的輔助碼組
+                    if charAuxCodes and lastChar:match('[' .. charAuxCodes .. ']') then -- 輔助碼存在
+                        found = true
+                        -- log.error(inputCode .. ' => ' .. cand.text)
+                        yield(Candidate(cand.type, cand.start, cand._end + 1, cand.text, ''))
+                        hasMatched = true
+                    end
+                end
+            end
+
+            if not found then
+                table.insert(insertLater, cand)
+            end
+        end
+
+        if not hasMatched then
+            for _, cand in ipairs(insertLater) do
+                yield(cand)
+            end
+        end
+    end
+end
+
+function AuxFilter.fini(env)
+    -- env.notifier:disconnect()
+end
+
+return AuxFilter
